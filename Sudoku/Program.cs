f115fed4c498b9555e95f6f0d95999971992562a
Gerardo Rodriguez <Gerardo.Rodriguez.844@my.csun.edu>
2017-04-25 16:05:39 -0700

    Comboboxes done, working on parsing

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,7 +19,6 @@ namespace Sudoku
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SudokuTLP());
         }
-
 
     }
 }
--- Sudoku/SudokuTLP.cs
+++ Sudoku/SudokuTLP.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -10,56 +11,133 @@ namespace Sudoku
 {
     class SudokuTLP : Form
     {
+
+
         public SudokuTLP()
         {
             TableLayoutPanel p = new TableLayoutPanel();
-            Button btn = new System.Windows.Forms.Button();
-            // 
-            // tableLayoutPanel1
-            // 
+
             p.ColumnCount = 9;
+            p.RowCount = 9;
+
             for (int i = 0; i < p.ColumnCount; i++)
             {
                 ColumnStyle c = new ColumnStyle();
-
                 c.SizeType = SizeType.Absolute;
-                c.Width = 100f;
+                c.Width = 70f;
 
                 p.ColumnStyles.Add(c);
-
-                p.CellPaint += TLP_CellPaint;
-
             }
-            p.Location = new System.Drawing.Point(10, 10);
 
-            p.RowCount = 9;
+            p.Location = new System.Drawing.Point(30, 30);
+
             for (int i = 0; i < p.RowCount; i++)
             {
                 RowStyle r = new RowStyle();
-
                 r.SizeType = SizeType.Absolute;
-                r.Height = 100f;
+                r.Height = 70f;
+
                 p.RowStyles.Add(r);
             }
 
-            p.Size = new System.Drawing.Size(900, 900);
+            CallCombo(p);
+        
+            p.Size = new System.Drawing.Size(630, 630);
 
-         //   p.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
-
-            p.Controls.Add(btn, 3, 4);
-
-
-            btn.Text = "button1";
-            // 
-            // Form1
-            // 
-            this.ClientSize = new System.Drawing.Size(1000, 1000);
+            this.ClientSize = new System.Drawing.Size(700, 700);
             this.Controls.Add(p);
+
+            p.Paint += new PaintEventHandler(PaintPanel);
         }
 
-        private void TLP_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        public void CallCombo(TableLayoutPanel p)
         {
-            e.Graphics.DrawLine(Pens.Black, e.CellBounds.Location, new Point(e.CellBounds.Right, e.CellBounds.Top));
+            String[,] puzzle = ReadFile();
+
+            for (int row = 0; row < p.RowCount; row++)
+            {
+                for(int col = 0; col < p.ColumnCount; col++)
+                {
+                    AddComboBox(p, col, row);
+                    Console.Write(puzzle[row, col]);
+                }
+                Console.Write(Environment.NewLine);
+            }
         }
+        public void AddComboBox(TableLayoutPanel p, int c, int r)
+        {
+            ComboBox cb = new ComboBox();
+
+            cb.Height = 40;
+            cb.Width = 45;
+            cb.Anchor = (AnchorStyles.None);
+            cb.DropDownStyle = ComboBoxStyle.DropDownList;
+            cb.Font = new Font(FontFamily.GenericSansSerif, 15.0f, FontStyle.Regular);
+
+            cb.DisplayMember = "Text";
+            cb.ValueMember = "Value";
+
+            var num = new[] {
+                new {Text = " ", Value = "0" },
+                new {Text = "1", Value = "1" },
+                new {Text = "2", Value = "2" },
+                new {Text = "3", Value = "3" },
+                new {Text = "4", Value = "4" },
+                new {Text = "5", Value = "5" },
+                new {Text = "6", Value = "6" },
+                new {Text = "7", Value = "7" },
+                new {Text = "8", Value = "8" },
+                new {Text = "9", Value = "9" }
+            };
+
+            cb.DataSource = num;
+
+            p.Controls.Add(cb, c, r);
+        }
+
+
+        public String[,] ReadFile()
+        {
+            String file = @"C:\Users\geardo\Documents\GitHub\Sudoku_GUI\Sudoku\puzzle.txt";
+            String[] parsed;
+
+            String line = null;
+
+            String[,] items = new String[9, 9];
+            int j = 0;
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while(!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    parsed = line.Split(' ');
+                    
+                    for(int i = 0; i < items.GetLength(0); i++)
+                    {
+                        items[j, i] = parsed[i];
+                    }
+                    j++;
+                }
+            }
+            return items;
+        }
+
+        private void PaintPanel(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (Graphics g = e.Graphics)
+            {
+                Pen pen = new Pen(Color.DimGray, 5);
+
+
+                g.DrawLine(pen, 212, 0, 212, 630);
+                g.DrawLine(pen, 212 * 2, 0, 212 * 2, 630);
+
+                g.DrawLine(pen, 0, 212, 630, 212);
+                g.DrawLine(pen, 0, 212 * 2, 630, 212 * 2);
+            }
+        }
+
     }
 }
